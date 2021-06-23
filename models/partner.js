8594cb7a0f6eb00e26729ea99f33d2d8a296ac32
nareiqat <nabeelareiqat@gmail.com>
2021-06-23 14:41:00 -0400

    workshop week 2 done

---
+++ models/partner.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    featured: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Partner = mongoose.model("Partner", partnerSchema);
+
+module.exports = Partner;
