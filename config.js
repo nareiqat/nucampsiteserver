10490f9fc36ee2af3034493a7c9215dabe5e8f97
nareiqat <nabeelareiqat@gmail.com>
2021-07-08 15:02:54 -0400

    oauth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "7768202030146256",
+    clientSecret: "15404cbb1996f7a3a05215518a578062",
+  },
+};
